a21b6797481438564a86ec8faccc05a7d4be60f8
Shaik Raheem <91a447raheem@gmail.com>
2021-01-06 11:55:51 +0530

    Slider issue fix

--- src/components/Common/Slider.js
+++ src/components/Common/Slider.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ItemsCarousel from 'react-items-carousel';
 import {
     BatteryStatusIcon
 } from '../../Assets'
-import CarouselSlider from 'react-carousel-slider';
 
 const SliderBlock = () => {
+    const [activeItemIndex, changeActiveItem] = useState(2)
 
     const itemList = [{
         "title": "Cell Voltage (24)",
@@ -43,25 +44,46 @@ const SliderBlock = () => {
     }
     ]
 
-    const handleClick = () => {
-        alert("Clicked in Mid Slider")
+    const handleClick = (index) => {
+        console.log("Clicked in Mid Slider", index)
+        changeActiveItem(index)
     }
 
-    let items = itemList.map((item, index) => {
-        return (<div className="block" onClick={handleClick}>
-            <div className="title">{item.title}</div>
-            <div className="images"><img height={70} src={item.image} /></div>
-            <div className="subtitle">{item.subtitle}</div>
-        </div>)
-    }
-    );
-
+    let activeItem = activeItemIndex + 2
+    console.log("activeItem", activeItem, activeItemIndex)
     // refer this doc for CSS Change https://github.com/Carr1005/react-carousel-slider#accEle
 
     return (
-        <div className="slider">
-            <CarouselSlider slideCpnts={items} accEle={{ dots: false }} sliderBoxStyle={{ width: '100%' }} itemsStyle={{ margin: '0px 5px', minWidth: "200px" }} buttonSetting={{ placeOn: 'middle-outside' }} lBtnCpnt={<span class="material-icons">navigate_before</span>} rBtnCpnt={<span class="material-icons">navigate_next</span>} />
-        </div>
+        <ItemsCarousel
+            // Placeholder configurations
+            enablePlaceholder
+            // Carousel configurations
+            numberOfCards={5}
+            gutter={12}
+            showSlither={true}
+            firstAndLastGutter={true}
+            freeScrolling={true}
+
+            // Active item configurations
+            requestToChangeActive={changeActiveItem}
+            activeItemIndex={activeItemIndex}
+            activePosition={'center'}
+            // infiniteLoop={true}
+            chevronWidth={24}
+            rightChevron={<span class="material-icons">navigate_next</span>}
+            leftChevron={<span class="material-icons">navigate_before</span>}
+            outsideChevron={true}
+            alwaysShowChevrons={true}
+            slidesToScroll={1}
+        >
+            {itemList.map((item, index) => {
+                return (<div className="SingleSlide"><div className={activeItemIndex == index ? "block activeSlide" : "block"} onClick={() => handleClick(index)}>
+                    <div className="title">{item.title}</div>
+                    <div className="images"><img height={70} src={item.image} /></div>
+                    <div className="subtitle">{item.subtitle}</div>
+                </div></div>)
+            })}
+        </ItemsCarousel>
     );
 }
 
